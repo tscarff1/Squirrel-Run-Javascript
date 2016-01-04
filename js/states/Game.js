5e32771df01f42be4182166e1e56bdcb7e572742
unknown <tyler@brinkbit.com>
2016-01-04 03:17:19 -0500

    Added acorn spawning with pseudo-random position and speed. Basic score functions implemented

--- js/states/Game.js
+++ js/states/Game.js
@@ -1,3 +1,4 @@
+"use strict"
 BasicGame.Game = function(game){
 	
 }
@@ -5,10 +6,14 @@ BasicGame.Game = function(game){
 BasicGame.Game.prototype = {
 	preload: function(){
 		this.score = 0;
+		this.scoreMult = 1;
+		this.SCORETIMER = 30;
 		this.ROADHEIGHT = 3*this.game.height/5;
 		this.BOTTOM = 7*this.game.height/8;
 		this.JUMPBUTTON = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		this.canJump = false;
+		this.ACORNCHANCE = .005;
+		this.ACORNMINY = this.game.height/2;
 	},
 
 	create: function(){
@@ -49,16 +54,22 @@ BasicGame.Game.prototype = {
 
 		//set up physics
 		this.physics.startSystem(Phaser.Physics.ARCADE);
-		this.physics.arcade.gravity.y = 600;
+		this.physics.arcade.gravity.y = 800;
 		this.physics.arcade.enable([this.toon, this.botBound]);
 		//this.botBound.body.setSize(0,this.BOTTOM,this.game.width, this.BOTTOM+1);
 		this.botBound.body.immovable = true;
 		this.botBound.body.allowGravity = false;
 
+
+		//Now let's set up acorns
+		this.acorns = this.add.group();
+
 	//var bg = this.add.tileSprite(game,0,0,this.game.width, this.game.height,'Play_TA', 'hills');
 	},
 
 	update: function(){
+		this.updateScore();
+		console.log(this.score);
 		this.physics.arcade.collide(this.toon, this.botBound, this.landing, null, this);
 
 		if(this.JUMPBUTTON.isDown && this.canJump){
@@ -66,12 +77,51 @@ BasicGame.Game.prototype = {
 			this.canJump = false;
 			this.toon.animations.stop(null, true);
 		}
+
+		//----- ACORN METHODS -----
+		//Should we spawn acorns?
+		if(Math.random() < this.ACORNCHANCE)
+			this.spawnAcorn(this);
+		//check for acorn collisons
+		var self = this;
+		this.acorns.forEach(function(item){
+			if(Phaser.Rectangle.intersects(self.toon, item)){
+				self.scoreMult++;
+				item.destroy();
+			}
+		});
 	},
 
+	// ----- TOON BASED FUNCTIONS -----
 	landing: function(toon, ground){
 		if(!this.canJump){
 			this.canJump = true;
 			this.toon.animations.play('running');
 		}
+	},
+
+	//----- ACORN FUNCTIONS -----
+	spawnAcorn: function(self){
+		var acornY = self.ACORNMINY + (Math.random() * (this.BOTTOM - self.ACORNMINY - 50));
+
+		var acorn = self.add.sprite(self.game.width,acornY, 'Play_TA', 'acorn');
+		acorn.scale.setTo(.75,.75);
+		var travelTime = 3000 + Math.random() * 2000;
+		//move the acorn across the stage
+		var acornTween = this.add.tween(acorn).to({x: 0, y: acorn.position.y}, travelTime);
+		acornTween.onComplete.add(function(){
+			acorn.kill();
+			self.acorns.remove(acorn);	
+		}, this);
+		acornTween.start();
+		self.acorns.add(acorn);
+
+	},
+
+	updateScore: function(){
+		if(this.game.time.now % this.SCORETIMER == 0){
+			this.score += 1 * this.scoreMult;
+		}
 	}
+
 };
