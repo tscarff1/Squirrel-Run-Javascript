d9808a9c6ac28b5f563bcc00240166103941106b
unknown <tscarff1@gmail.com>
2016-01-11 03:09:42 -0500

    Baseball bounces and turns. Bounce has some random variation at spawn. Implemented sun and moon and sky tint. Began work on energy drink.

--- js/states/Game.js
+++ js/states/Game.js
@@ -11,7 +11,6 @@ BasicGame.Game.prototype = {
 		this.SCORETIMER = 10;
 		
 		this.ROADSPEED = 8000;
-		
 		this.ROADHEIGHT = 3*this.game.height/5;
 		this.BOTTOM = 7*this.game.height/8;
 		
@@ -21,10 +20,8 @@ BasicGame.Game.prototype = {
 		this.TOONBOX = [30,30,50,40];
 		this.TOONSPEED = 150;
 		this.canJump = false;
-
 		this.HURTTIME = 0;
 		this.HURTTIMER = 100;
-		
 		this.HYPERTIME = 0;
 		this.HYPERTIMER = 150
 
@@ -33,11 +30,17 @@ BasicGame.Game.prototype = {
 		
 		this.MOLECHANCE = 0.05;
 		this.MOLEBOX = [10,20,20,10];
+		this.BCHANCE = .005;
+		this.BBOX =[5,5,5,5]
+
+		this.SAMXRADIUS = this.game.width/2  +20;
+		this.SAMYRADIUS = this.game.height-20;
 	},
 
 	create: function(){
-		
 
+		//Set up the sun and moon (MUST be first)
+		this.setUpSAM();
 		//draw the background
 		//--- Road ---
 		var graphics = this.add.graphics(0, 0);
@@ -56,82 +59,44 @@ BasicGame.Game.prototype = {
 	    botGraphics.lineTo(this.game.width, this.BOTTOM);
 
 	    this.botBound = this.add.sprite(0, this.BOTTOM, botGraphics.generateTexture());
-
-
-
-		//Set up toon
-		this.toon = this.add.sprite(0,0,'Play_TA', 'Toon_Running_1');
-		this.toon.position.setTo(50,150);
-		this.toon.animations.add('running', Phaser.Animation.generateFrameNames('Toon_Running_',1,3),3, true);
-		this.toon.animations.add('jumping', 'Toon_Running_1', 2, true);
-		this.toon.animations.play('running', 12, true);
-		this.toon.isHurt = false;
-		this.toon.isHyper = false;
 		
+		this.setupToon();
+		this.setupDrink();
 		this.setupEnemies();
-
-		//set up physics
-		this.physics.startSystem(Phaser.Physics.ARCADE);
-		this.physics.arcade.gravity.y = 800;
-		this.physics.arcade.enable([this.toon, this.botBound]);
-		this.toon.body.collideWorldBounds = true;
-		this.toon.scale.setTo(.7,.7);
-		this.toon.hitbox = new Phaser.Rectangle(this.TOONBOX[0], this.TOONBOX[1],
-			this.toon.width - this.TOONBOX[2] ,this.toon.height - this.TOONBOX[3]);
-
+		this.setupPhysics();
 		
-
-		this.botBound.body.immovable = true;
-		this.botBound.body.allowGravity = false;
-
-
 		//Now let's set up acorns
 		this.acorns = this.add.group();
 		//That was easy!
 	
-		//place the Display stuff
-		var panel = this.add.sprite(0,0,'UI_TA', 'panel');
-		panel.anchor.setTo(.5,.5);
-		panel.position.setTo(6*this.stage.width/11 - panel.width/2, this.game.height/10);
-		panel.scale.setTo(2.7,1.6);
-
-		var hyperPanel = this.add.sprite(250,55,'UI_TA', 'Hyper Bar Panel');
-		this.hyperBar = this.add.sprite(261, 58, 'UI_TA', 'Hyper Bar Meter');
-		this.scoreText = this.add.bitmapText(250, 10, 'zantroke', 'Score: 0', 30);
+		//Has to go last so that the display is on top
+		this.setupDisplay();
 	},
 
 	update: function(){
 		this.updateHitboxes();
 
 		this.updateScore();
+		this.updateSAM();
+
 		this.moleStarter();
+		this.baseballStarter();
 		this.mole.updateCrop();
-		//console.log(this.score);
 		this.physics.arcade.collide(this.toon, this.botBound, this.landing, null, this);
 		this.moveToon();
 		
 
 		//----- ACORN METHODS -----
 		//Should we spawn acorns?
-		if(Math.random() < this.ACORNCHANCE)
-			this.spawnAcorn(this);
-		//check for acorn collisons
-		this.acorns.forEach(function(item){
-			if(Phaser.Rectangle.intersects(this.toon.hitbox, item)){
-				this.scoreMult++;
-				item.destroy();
-			}
-		}.bind(this));
+		this.acornSpawner();
 
-		//Check for enemy collisions
-		this.enemies.forEach(function(enemy){
-				if(Phaser.Rectangle.intersects(this.toon.hitbox, enemy.hitbox)){
-					this.resetMole();
-					this.toon.animations.stop(null, true);
-					this.toon.frameName = 'Toon_Tripping';
-					this.toon.isHurt = true;
-				}
-		}.bind(this));
+		this.checkCollisions();
+
+		//----- ENEMY STUFF -----
+		if(this.baseball.position.x < this.game.width +10)
+			this.baseball.angle--;
+
+		
 
 		if(this.toon.isHurt){
 			if(this.HURTTIME > this.HURTTIMER){
@@ -141,21 +106,69 @@ BasicGame.Game.prototype = {
 			}
 			this.HURTTIME++;
 		}
-		//console.log(this.moleEnemy.getBounds());
 	},
 
-	// Comment this out when testing 'final' versions of game
+	/*// Comment this out when testing 'final' versions of game
 	render: function(){
 		game.debug.geom( this.toon.hitbox, 'rgba(255,0,0, .7)' ) ;
 		game.debug.geom( this.moleEnemy.hitbox, 'rgba(0,255,0, .7)' ) ;
+		game.debug.geom( this.baseball.hitbox, 'rgba(0,255,0, .7)' );
 		
 	},
+*/
+	//----- SETUP FUNCTIONS -----
+	setupToon: function(){
+		this.toon = this.add.sprite(0,0,'Play_TA', 'Toon_Running_1');
+		this.toon.position.setTo(50,150);
+		this.toon.animations.add('running', Phaser.Animation.generateFrameNames('Toon_Running_',1,3),3, true);
+		this.toon.animations.add('jumping', 'Toon_Running_1', 2, true);
+		this.toon.animations.play('running', 12, true);
+		this.toon.isHurt = false;
+		this.toon.isHyper = false;
+	},
 
-	//----- Setup Functions -----
+	setupDrink: function(){
+		this.drink = this.add.sprite(0,0,'Play_TA','EnergyDrink');
+		this.drink.scale.setTo(.6,.6);
+		this.drink.angle = -20;
+	},
+
+	setupPhysics: function(){
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+		this.physics.arcade.gravity.y = 800;
+		this.physics.arcade.enable([this.toon, this.botBound]);
+		this.toon.body.collideWorldBounds = true;
+		this.toon.scale.setTo(.7,.7);
+		this.toon.hitbox = new Phaser.Rectangle(this.TOONBOX[0], this.TOONBOX[1],
+			this.toon.width - this.TOONBOX[2] ,this.toon.height - this.TOONBOX[3]);
+
+		this.botBound.body.immovable = true;
+		this.botBound.body.allowGravity = false;
+	},
+
+	setupDisplay: function(){
+		var panel = this.add.sprite(0,0,'UI_TA', 'panel');
+		panel.anchor.setTo(.5,.5);
+		panel.position.setTo(this.stage.width/2 - panel.width/2, this.game.height/10);
+		panel.scale.setTo(2.7,1.6);
+
+		var hyperPanel = this.add.sprite(220,55,'UI_TA', 'Hyper Bar Panel');
+		this.hyperBar = this.add.sprite(231, 58, 'UI_TA', 'Hyper Bar Meter');
+		this.scoreText = this.add.bitmapText(220, 10, 'zantroke', 'Score: 0', 30);
+	},
+
+	setUpSAM: function(){
+		this.sun = this.add.sprite(100,0,'Play_TA','sun');
+		this.sun.anchor.x = .5;
+		this.moon = this.add.sprite(0,0, 'Play_TA', 'moon');
+		this.moon.anchor.x=.5;
+		this.samAngle = 0;
+	},
+
 	setupEnemies: function(){
 		this.enemies = this.add.group();
 		this.setupMole();
-
+		this.setupBaseball();
 	},
 
 	setupMole: function(){
@@ -168,6 +181,7 @@ BasicGame.Game.prototype = {
 		this.moleEnemy.add(this.mole);
 		this.moleEnemy.add(this.dirt);
 		this.enemies.add(this.moleEnemy);
+		this.moleEnemy.isReset = true;
 
 		this.moleEnemy.hitbox = new Phaser.Rectangle(this.MOLEBOX[0] + this.moleEnemy.position.x,
 			this.MOLEBOX[1] + this.BOTTOM -this.moleEnemy.height,
@@ -176,7 +190,22 @@ BasicGame.Game.prototype = {
 		this.moleCrop = new Phaser.Rectangle(0,0, this.mole.width, 10);
 		//this.mole.crop(this.moleCrop);
 		this.moleCropTween = this.add.tween(this.moleCrop).to({height: this.mole.height}, 2000);
-	
+	},
+
+	setupBaseball: function(){
+		this.baseball = this.add.sprite(0, 0, 'Play_TA', 'baseball');
+		this.baseball.hitbox = new Phaser.Rectangle(this.baseball.position.x + this.BBOX[0],
+													this.baseball.position.y + this.BBOX[1],
+													this.baseball.width - this.BBOX[0] - this.BBOX[2],
+													this.baseball.height - this.BBOX[1] - this.BBOX[3]);
+		this.baseball.position.x = this.game.width+10;
+		this.baseball.position.y = this.game.height/2- this.baseball.height;
+		this.baseball.anchor.setTo(.5,.5);
+		this.enemies.add(this.baseball);
+		this.xOffset = 20 + Math.floor(Math.random() * 30);
+		this.baseball.position.x = this.game.width + this.baseball.width + 20;
+		this.baseball.position.y = this.game.height/2+20;
+		this.baseball.isReset = true;
 	},
 
 	// ----- TOON BASED FUNCTIONS -----
@@ -212,21 +241,45 @@ BasicGame.Game.prototype = {
 	},
 
 	//----- ACORN FUNCTIONS -----
-	spawnAcorn: function(self){
-		var acornY = self.ACORNMINY + (Math.random() * (this.BOTTOM - self.ACORNMINY - 50));
+	acornSpawner: function(){
+		if(Math.random() < this.ACORNCHANCE)
+			this.spawnAcorn();
+	},
 
-		var acorn = self.add.sprite(self.game.width,acornY, 'Play_TA', 'acorn');
+	spawnAcorn: function(){
+		var acornY = this.ACORNMINY + (Math.random() * (this.BOTTOM - this.ACORNMINY - 50));
+
+		var acorn = this.add.sprite(this.game.width,acornY, 'Play_TA', 'acorn');
 		acorn.scale.setTo(.75,.75);
 		var travelTime = 3000 + Math.random() * 2000;
 		//move the acorn across the stage
 		var acornTween = this.add.tween(acorn).to({x: 0, y: acorn.position.y}, travelTime);
 		acornTween.onComplete.add(function(){
 			acorn.kill();
-			self.acorns.remove(acorn);	
+			this.acorns.remove(acorn);	
 		}, this);
 		acornTween.start();
-		self.acorns.add(acorn);
+		this.acorns.add(acorn);
 
+	},
+
+	//----- Update functions -----
+	updateSAM: function(){
+		//Makes the background darker as the sun goes down and lighter as it comes up
+		//Note the three decimals:
+		//1st: Larger this is, the lighter the sky will be overall, and the sky is darker when this is smaller
+		//2nd: Larger = greater light range, smaller = less light range
+		//3rd: Similar to the first
+		var bgColorMod =(.95- (this.sun.position.y + this.game.height)/(2 * this.game.height)*.8) + .5;
+		var g = Math.floor((parseInt('6f',16) * bgColorMod)).toString(16);
+		var b = Math.floor((parseInt('e6',16) * bgColorMod)).toString(16);
+		this.game.stage.backgroundColor = '#00' + g + b;
+		this.sun.position.x = this.game.width/2 + Math.cos(this.samAngle * Math.PI/180) * this.SAMXRADIUS;
+		this.sun.position.y = this.game.height + Math.sin(this.samAngle * Math.PI/180) * this.SAMYRADIUS;
+
+		this.moon.position.x = (this.game.width/2 - this.sun.position.x) + this.game.width/2;
+		this.moon.position.y = (this.game.height - this.sun.position.y) + this.game.height;
+		this.samAngle-=.12;
 	},
 
 	updateScore: function(){
@@ -245,24 +298,76 @@ BasicGame.Game.prototype = {
 		this.toon.hitbox.x = this.toon.position.x + this.TOONBOX[0];
 		this.toon.hitbox.y = this.toon.position.y + this.TOONBOX[1];
 		this.moleEnemy.hitbox.x = this.moleEnemy.position.x + this.MOLEBOX[0] + this.game.width;
+		this.baseball.hitbox.x = this.baseball.position.x + this.BBOX[0] -  this.baseball.width/2;
+		this.baseball.hitbox.y = this.baseball.position.y + this.BBOX[1] - this.baseball.height/2;
 	},
 
-	//Enemy Functions
-	//----- Mole Functions -----
-	resetMole: function() {
-		this.moleEnemy.moveTween.stop();
-		this.moleEnemy.position.x = 0;
+	checkCollisions: function(){
+		//Check for enemy collisions
+		this.enemies.forEach(function(enemy){
+				if(Phaser.Rectangle.intersects(this.toon.hitbox, enemy.hitbox)){
+					if(enemy == this.moleEnemy)
+						this.resetMole();
+					else if(enemy == this.baseball)
+						this.resetBaseball();
+					this.toon.animations.stop(null, true);
+					this.toon.frameName = 'Toon_Tripping';
+					this.toon.isHurt = true;
+				}
+		}.bind(this));
 
+		//check for acorn collisons
+		this.acorns.forEach(function(item){
+			if(Phaser.Rectangle.intersects(this.toon.hitbox, item)){
+				this.scoreMult++;
+				item.destroy();
+			}
+		}.bind(this));
+	},
+
+	//----- ENEMY FUNCTIONS -----
+	resetMole: function() {
+		if(this.moleEnemy.moveTween != undefined)
+			this.moleEnemy.moveTween.stop();
+		this.moleEnemy.position.x = 0;
+		this.moleEnemy.isReset = true;
 		//this.moleCrop.height = 0;
 		
 	},
 
 	moleStarter:function(){
-		if(Math.random() < this.MOLECHANCE && this.moleEnemy.position.x >= 0){
+		if(Math.random() < this.MOLECHANCE && this.moleEnemy.isReset == true){
+			this.moleEnemy.isReset = false;
 			this.moleEnemy.moveTween = this.add.tween(this.moleEnemy).to({x:-1 * (this.game.width + this.mole.width + 100)}, this.ROADSPEED);
 			this.moleEnemy.moveTween.onComplete.add(this.resetMole, this);
 			this.moleEnemy.moveTween.start();
 		}
-	}
+	},
+
+	baseballStarter: function(){
+		if(Math.random() < this.BCHANCE && this.baseball.isReset == true){
+			console.log("starting baseball");
+			this.baseball.isReset = false;
+
+			this.baseball.moveTime = this.ROADSPEED + Math.floor(Math.random() * 400)-200;
+			this.baseball.xTween = this.add.tween(this.baseball).to({x:0}, this.ROADSPEED);
+			this.baseball.xTween.onComplete.add(this.resetBaseball, this);
+			this.baseball.xTween.start();
+			
+			this.baseball.yTween = this.add.tween(this.baseball).to({y: [this.BOTTOM - this.baseball.height/2,this.game.height/2 +20]}, 
+				1000,"Sine.easeInOut", true, 0 ,-1);
+			this.baseball.yTween.start();
+		}
+	},
+
+	resetBaseball: function(){	
+		this.baseball.xTween.stop();
+		this.baseball.yTween.stop();
+		this.xOffset = 20 + Math.floor(Math.random() * 30);
+		
+		this.baseball.position.y = this.game.height/2+20;
+		this.baseball.position.x = this.game.width + this.baseball.width + 20;
+		this.baseball.isReset = true;
+	},
 
 };
--- js/states/MainMenu.js
+++ js/states/MainMenu.js
@@ -9,7 +9,7 @@ BasicGame.MainMenu.prototype = {
     },
 
     create: function() {
-        
+        this.game.stage.backgroundColor = '#006fe6';
         this.add.sprite(0,0, 'UI_TA').frameName ='Toon the Squirrel Standing';
         var title = this.add.sprite(100,100, 'UI_TA');
         title.frameName ='Title';
@@ -26,14 +26,22 @@ BasicGame.MainMenu.prototype = {
         this.startTxt = this.add.bitmapText(this.startBtn.position.x-5, this.startBtn.position.y, 'zantroke', 'START', 30);
         this.startTxt.anchor.setTo(.5,.5);
 
-        this.startGroup = this.add.group();
-        this.startGroup.add(this.startBtn);
-        this.startGroup.add(this.startTxt);
-
         this.startBtn.onInputOver.add(this.tiltButton, this, 0);
         this.startBtn.onInputOut.add(this.stopTilt, this, 0);
 
-        this.tiltTween;
+        this.tiltTween1;
+        this.tiltTween2;
+
+        this.instrBut = this.game.add.button(0,0, 'UI_TA', this.onClickFunction, this, 'button', 'button', 'button');
+        this.instrBut.anchor.setTo(.5,.5);
+        this.instrBut.position.setTo( 6* this.game.width/8 - this.instrBut.width/2,
+                                      7 * this.game.height/8 - this.instrBut.height/2);
+
+        this.instrTxt = this.game.add.bitmapText(this.instrBut.position.x -5, this.instrBut.position.y,
+         'zantroke', 'INSTRUCTIONS', 18);
+        this.instrTxt.anchor.setTo(.5,.5);
+        this.instrBut.onInputOver.add(this.tiltButton, this, 0);
+        this.instrBut.onInputOut.add(this.stopTilt, this, 0);
 
     },
 
@@ -43,26 +51,42 @@ BasicGame.MainMenu.prototype = {
     },
 
     onClickFunction: function(btn){
-        this.game.state.start('Game');
+        if(btn == this.startBtn)
+         this.game.state.start('Game');
     },
 
     tiltButton: function(btn){
         var txt;
-        if(btn = this.startBtn)
+        if(btn == this.startBtn)
             txt = this.startTxt;
+        else if(btn == this.instrBut)
+            txt = this.instrTxt;
 
-        this.tiltTween = this.add.tween(btn).to({angle: -20}, 2000,
+        this.tiltTween1 = this.add.tween(btn).to({angle: -20}, 2000,
             function(t){
                 return Math.sin(Math.PI * 2 * t);
             }, true, 0, -1);
-        tiltTween.start();
+        this.tiltTween1.start();
     
-        this.add.tween(txt).to({angle: -20}, 2000,
+        this.tiltTween2 = this.add.tween(txt).to({angle: -20}, 2000,
             function(t){
                 return Math.sin(Math.PI * 2 * t);
-            }, true, 0, -1).start();
+            }, true, 0, -1);
+        this.tiltTween2.start();
     },
 
     stopTilt: function(btn){
+        var txt;
+        if(btn == this.startBtn)
+            txt = this.startTxt;
+        else if(btn == this.instrBut)
+            txt = this.instrTxt;
+        this.tiltTween1.stop();
+        this.tiltTween2.stop();
+        stopTween = this.add.tween(btn).to({angle: 0},100);
+        txtStopTween = this.add.tween(txt).to({angle: 0}, 100);
+        txtStopTween.start();
+        stopTween.start();
+        
     }
 };
